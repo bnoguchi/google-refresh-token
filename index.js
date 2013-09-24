9d6ce2566eb0f13be1f742a1b75bb32c30389838
Brian Noguchi <brian.noguchi@gmail.com>
2013-09-24 01:47:47 -0700

    Add and expose method checkTokenValidity for checking if an access token is valid

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var request = require('request');
  *   application registration.
  * @param {Function} cb(err, {accessToken, expiresIn, idToken}, response);
  */
-module.exports = function refreshToken (refreshToken, clientId, clientSecret, cb) {
+exports = module.exports = function refreshGoogleToken (refreshToken, clientId, clientSecret, cb) {
   request.post('https://accounts.google.com/o/oauth2/token', {
     form: {
       refresh_token: refreshToken
@@ -32,6 +32,33 @@ module.exports = function refreshToken (refreshToken, clientId, clientSecret, cb
       , expiresIn: body.expires_in
       , idToken: body.id_token
       }, res);
+    }
+  });
+};
+
+exports.checkTokenValidity = function (accessToken, refreshToken, clientId, clientSecret, cb) {
+  request({
+    url: 'https://www.googleapis.com/oauth2/v1/userinfo'
+  , qs: {alt: 'json'}
+  , json: true
+  , headers: { Authorization: 'Bearer ' + accessToken }
+  }, function (err, res, json) {
+    if (err) return cb(err);
+    if (json.error && json.error.message === 'Invalid Credentials') {
+      refreshGoogleToken(refreshToken, clientId, clientSecret, function (err, json, res) {
+        if (!err && json.error) {
+          err = new Error(res.statusCode + ': ' + json.error.message);
+        }
+        var accessToken = json.accessToken;
+        if (!err && !accessToken) {
+          err = new Error(res.statusCode + ': refreshToken error');
+        }
+        if (err) return cb(err);
+        var expireAt = +new Date + parseInt(json.expiresIn, 10);
+        cb(null, false, accessToken, expireAt);
+      });
+    } else {
+      cb(null, true);
     }
   });
 };
